ef5676df0d0653857deac7f4021657141a44eb27
Rhana <159554135+RHAN4@users.noreply.github.com>
2025-02-11 09:20:52 -0300

    Add files via upload

---
+++ Array02.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+public class Array02 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double[] notas = new double[2];
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Digite a " + (i + 1) + "° nota: ");
+            notas[i] = ler.nextDouble();
+        }
+            System.out.println("\nExibindo as notas: ");
+            for (double nota : notas) {
+                System.out.println("Nota: " + nota);
+            }
+        ler.close();
+    }
+}
